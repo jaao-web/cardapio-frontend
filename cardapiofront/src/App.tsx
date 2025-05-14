72c2c5fcc60416138407ed95e6494fd65525859d
João Vitor Garcia Andrade <80601634+jaao-web@users.noreply.github.com>
2025-05-14 15:52:41 -0300

    botão 'novo' funcionando

--- cardapiofront/src/App.tsx
+++ cardapiofront/src/App.tsx
@@ -1,23 +1,32 @@
+import { useState } from 'react'
 import './App.css'
 import { Card } from './components/card/card';
-import { useFoodData } from './hooks/useFoodData';
 import type { FoodData } from './interface/FoodData';
+import { useFoodData } from './hooks/useFoodData';
+import { CreateModal } from './create-modal/create-modal'
 
 function App() {
   const { data } = useFoodData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(prev => !prev)
+  }
 
   return (
     <div className="container">
       <h1>Cardápio</h1>
-      <div className='card-grid'>
-        {data?.map(foodData =>
+      <div className="card-grid">
+        {data?.map(foodData => 
           <Card
-            price={foodData.price}
-            title={foodData.title}
+            price={foodData.price} 
+            title={foodData.title} 
             image={foodData.image}
           />
         )}
       </div>
+      {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
+      <button onClick={handleOpenModal}>novo</button>
     </div>
   )
 }
